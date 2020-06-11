e11bb15c1a62aadbb72684f7bffaaa63825ea067
王咸洋 <18521739316@163.com>
2020-06-11 11:04:45 +0800

    first commit

---
+++ rpa-recruitment/php/test/MyGreeter_Client_Test.php
@@ -0,0 +1,28 @@
+<?php
+
+class MyGreeter_Client_Test extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->greeter = new \MyGreeter\Client();
+    }
+
+    public function test_Instance()
+    {
+        $this->assertEquals(
+            get_class($this->greeter),
+            'MyGreeter\Client'
+        );
+    }
+
+    public function test_getGreeting()
+    {
+        $this->assertTrue(
+            strlen($this->greeter->getGreeting()) > 0
+        );
+    }
+
+    public function test_outGreeting(){
+        echo $this->greeter->getGreeting();
+    }
+}
